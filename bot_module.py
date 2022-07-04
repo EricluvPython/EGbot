39b782b97cab0f136683a9a91b1f42370e38cf86
EricluvPython <59160410+EricluvPython@users.noreply.github.com>
2022-07-04 20:08:10 +0800

    Add files via upload

---
+++ bot_module.py
@@ -0,0 +1,35 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import sys,os
+from contextlib import contextmanager
+
+@contextmanager
+def suppress_stdout():		# this is to repress the bot training log
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+def train_bot(bot_name,bot_corpus="chatterbot.corpus.english"):	# trains the bot
+	chatbot = ChatBot(bot_name)
+	trainer = ChatterBotCorpusTrainer(chatbot)
+	trainer.train(bot_corpus)
+	return chatbot
+
+def get_response(question,trained_bot):	# get response from the bot
+	response = trained_bot.get_response(question)
+	return response
+
+if __name__ == "__main__":
+	# usage: python3 bot_module.py [QUESTION]
+	# input: 1 arg: question to be asked
+	# output: 1 string: answer to question
+	with suppress_stdout():
+		mybot = train_bot('EGbot')
+	question = sys.argv[1]	# get the first argument [QUESTION]
+	response = get_response(question,mybot)
+	print(response)
+	sys.exit(0)
---
+++ imap_receiver_module.py
@@ -0,0 +1,65 @@
+import getpass, email, sys
+from imapclient import IMAPClient
+import re, sys
+
+def imap_receive(hostname='imap.163.com',username='eric_g_0408@163.com',passwd='RAZLUTAYTGLGACDG'):
+	c = IMAPClient(hostname, ssl= True)  # create client object
+	try:
+		c.login(username, passwd)   # login to account
+	except c.Error:
+		print('Could not log in')   # in case anything goes wrong
+		sys.exit(1)
+	else:
+		c.id_({"name": "IMAPClient", "version": "2.1.0"})   # to avoid "unsafe login" rejection
+		c.select_folder('INBOX', readonly = False)          # load the mail inbox
+		result = c.search('UNSEEN')                         # select the unseen mails
+				
+		msgdict = c.fetch(result, ['BODY.PEEK[]'] )	# a dictionary of the mail content to be processed
+			
+		
+		for message_id, message in msgdict.items():     # this is set in case multiple emails are present
+						
+			message_decoded = {}			# decode dictionary from binary to utf8
+			for key, value in message.items():      # iteration over all items
+				try:
+					new_key = key.decode('utf-8')       # decode the message to utf8
+				except AttributeError:                  # in case anything is already in utf8
+					new_key = key
+				try:
+					new_value = value.decode('utf-8')
+				except AttributeError:
+					new_value = value
+				message_decoded[new_key] = new_value    # write decoded dictionary
+			
+			e = email.message_from_string(message_decoded['BODY[]'])                        # to find the value of the body			
+			subject = email.header.make_header(email.header.decode_header(e['SUBJECT']))    # to find the email subject
+			mail_from = email.header.make_header(email.header.decode_header(e['From']))     # to find the email sender			
+			mail_from = ''.join(re.findall('<.*?>',str(mail_from)))[1:-1]			# match the email address
+			
+			maintype = e.get_content_maintype()     # in case the mail wasn't plain text
+			if maintype == 'multipart':
+				for part in e.get_payload():        # basically only take the text part
+					if part.get_content_maintype() == 'text':
+						mail_content = part.get_payload(decode=True).strip()
+			elif maintype == 'text':
+				mail_content = e.get_payload(decode=True).strip()        
+			
+			mail_content = mail_content.decode('utf-8')  # decode the message
+			dir1 = re.search('<div>',mail_content).span()[1]    # locate the first <div>
+			dir2 = re.search('</div>',mail_content).span()[0]   # locate the first </div>
+			mail_body = mail_content[dir1:dir2]  # print content between the first div tags
+	
+	c.add_flags(result, br'\Seen')	# mark emails as read
+	c.logout()      		# logout
+	
+	try:
+		return str(mail_from), str(mail_body), str(subject)
+	except:
+		return "","",""		# otherwise an error occurs when no new emails are found
+
+if __name__ == "__main__":
+	# usage: python3 imap_receiver_module.py
+	# input: none
+	# output: 3 strings: mail sender address, mail body, mail subject
+	print(imap_receive())
+	sys.exit(0)
